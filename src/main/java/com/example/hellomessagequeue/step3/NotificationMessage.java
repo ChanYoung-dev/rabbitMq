012d0eab596d24e3150f58fb482149a1bd196b4e
ChanYoung-dev <emrhssla@gmail.com>
2026-01-15 19:25:54 +0900

    step3

---
+++ src/main/java/com/example/hellomessagequeue/step3/NotificationMessage.java
@@ -0,0 +1,20 @@
+package com.example.hellomessagequeue.step3;
+
+public class NotificationMessage {
+  private final String message;
+
+  // 기본 생성자 (필수)
+  public NotificationMessage() {
+    message = "";
+  }
+
+  // 선택
+  public NotificationMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+}
